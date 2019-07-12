57178109c5561b661c4b7896b20c772407299002
Szabolcs Nothnagel <szabolcs.nothnagel@dealogic.com>
2019-07-12 18:04:32 +0200

    Initial commit
    Added main window mockup
    Added models

---
+++ FileStasher/Model/FileStash.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStasher.Model
+{
+    public class FileStash
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; private set; }
+        public ObservableCollection<StashedFile> Files { get; set; }
+    }
+}
---
+++ FileStasher/Model/StashedFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace FileStasher.Model
+{
+    public class StashedFile
+    {
+        public string Name { get; set; }
+        public string SourcePath { get; set; }
+        public string LastRestorePath { get; set; }
+        public int StasherType { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+        public string Description { get; set; }
+        public ImageSource Icon { get; set; }
+    }
+}
