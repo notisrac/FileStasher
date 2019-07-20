040036e2487b713bb971f3800d0345e514362e84
Szabolcs Nothnagel <szabolcs.nothnagel@dealogic.com>
2019-07-20 16:23:10 +0200

    Added loading of application state
    Added loading file from command line

--- FileStasher/App.xaml.cs
+++ FileStasher/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using NLog;
+﻿using FileStasher.Model;
+using NLog;
 using NLog.Fluent;
 using SimpleInjector;
 using SimpleInjector.Lifestyles;
@@ -17,33 +18,55 @@ namespace FileStasher
     /// </summary>
     public partial class App : Application
     {
+        // the location where the app state file should be saved
+        private readonly string APPSTATE_FILEPATH = ""; // TODO add a location
+
         /// <summary>
         /// The IoC container
         /// </summary>
-        public static Container container;
+        public Container container { get; private set; }
+        /// <summary>
+        /// This stores the state of the application
+        /// </summary>
+        public ApplicationState AppState { get; private set; }
 
         protected override void OnStartup(StartupEventArgs e)
         {
             // instantiate the container
-            container = new Container();
-            container.Options.DefaultLifestyle = new AsyncScopedLifestyle();
+            this.container = new Container();
+            //this.container.Options.DefaultLifestyle = new ThreadScopedLifestyle();
             // register the types
-            container.Register<ILogger>(() => {
+            this.container.Register<ILogger>(() => {
                 return NLog.LogManager.GetLogger("default");
             });
 
+            // try to load the application state
+            this.AppState = ApplicationState.Load(APPSTATE_FILEPATH, true);
+
             base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // save the application state
+            this.AppState.Save();
+
+            base.OnExit(e);
         }
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             var mainWindow = container.GetInstance<MainWindow>();
+            //Application.Current.MainWindow = mainWindow;
 
             if (e.Args.Length == 1)
-            {
-                // TODO open file from command line argument
+            { // open the file from the command line
+                mainWindow.Initialize(e.Args[0]);
             }
-
+            else if (!string.IsNullOrWhiteSpace(this.AppState.LastOpenFile))
+            {
+                mainWindow.Initialize(this.AppState.LastOpenFile);
+            }
             mainWindow.Show();
         }
     }
--- FileStasher/MainWindow.xaml.cs
+++ FileStasher/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using FileStasher.Model;
+using Microsoft.Win32;
 using NLog;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,13 +25,110 @@ namespace FileStasher
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly ObservableCollection<FileStash> stashList = new ObservableCollection<FileStash>();
+        private StashContainer stashContainer;
         private readonly ILogger logger;
+        public string CurrentlyOpenFileName { get; set; }
+
 
         public MainWindow(ILogger logger)
         {
             InitializeComponent();
             this.logger = logger;
+            this.stashContainer = new StashContainer();
+            this.CurrentlyOpenFileName = string.Empty;
+        }
+
+        public void Initialize(string fileName)
+        {
+            this.Load(fileName);
+        }
+
+        public void New()
+        {
+            // TODO show the file open dialog, and get the filename from it
+            this.stashContainer = new StashContainer();
+            this.CurrentlyOpenFileName = "c:\test.json";
+        }
+
+        public void Unload()
+        {
+            // TODO check if there is a container loaded, or modified
+            // TODO if the collection was modified, ask to save
+            this.stashContainer = null;
+            this.CurrentlyOpenFileName = null;
+        }
+
+        public void Load(string fileName)
+        {
+            this.Unload();
+            if (!string.IsNullOrWhiteSpace(fileName))
+            { // try to load the file
+                this.CurrentlyOpenFileName = fileName;
+                try
+                {
+                    this.stashContainer = StashContainer.LoadFromFile(fileName, logger);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Could not load file \"{fileName}\": \r\n{ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                this.stashContainer = new StashContainer();
+                this.CurrentlyOpenFileName = null;
+            }
+        }
+
+        public void Save(string fileName)
+        {
+            this.stashContainer.SaveToFile(fileName);
+            // this is the save as functionality
+            if (0 != string.Compare(fileName, this.CurrentlyOpenFileName))
+            {
+                this.CurrentlyOpenFileName = fileName;
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            this.Unload();
+
+            base.OnClosing(e);
+        }
+
+        private void New_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.New();
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            // TODO show file loading dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog() {
+                Filter = "Stash containers (*.json)|*.json|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                Multiselect = false
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                this.Load(openFileDialog.FileName);
+            }
+        }
+
+        private void Close_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveAs_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
         }
     }
 }
